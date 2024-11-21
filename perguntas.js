805da9c3e0f5741a6a44687804a5f4a7dcebb9fd
urubudopixF <164928894+urubudopixF@users.noreply.github.com>
2024-11-21 11:37:32 -0300

    s

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Programação',
+    'Oque é javaScript',
+    'O JavaScript é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Franaça',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programção',
+    'Oque é uam função',
+    'Uma função é um bloco de código que exerce alguma tarefa'
+)
