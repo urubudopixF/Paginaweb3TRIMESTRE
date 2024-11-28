e7a81a987f468d1adc29a146b4205ef49c176e5a
urubudopixF <164928894+urubudopixF@users.noreply.github.com>
2024-11-28 11:48:45 -0300

    s

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 function criaCartao(categoria, pergunta, resposta) {
-    console.log
+    let container = document.getElementById('container')
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao'
+
+    cartao.innerHTML = `
+        <div class="cartao__conteudo">
+                    <h3>Programação</h3>
+                    <div class="cartao__conteudo__pergunta">
+                        <p>O que é JavaScript?</p>
+                    </div>
+                    <div class="cartao__conteudo__resposta">
+                        <p>O JavaScript é uma linguagem de programação</p>
+                    </div>
+                    </div>
+    `
 }
